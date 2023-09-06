b90392dcd5af38a91c6ae38ebdbff7f8c28999bb
Samine Dylah <samine@saminedylah.com>
2023-09-06 07:11:53 +0100

    entity display structure v6

--- pages/entity.tsx
+++ pages/entity.tsx
@@ -1,27 +1,31 @@
-
-import { View } from 'react-native';
-import { ViewProcessesTitle} from '../components/entity/processTabs'
-import { ViewDisplayMain, ViewDisplayActions, ViewDisplayTabs } from'../utils/display'
 import React from 'react';
-  
+import { View } from 'react-native';
+import { ViewDisplayMain, ViewDisplayTabs } from'../utils/display'
+import { ViewActionTabs } from'../utils/action'
+import { ViewFocusMain } from'../utils/focus'
+import { ViewWidgetMain } from '../utils/widget'
+import { useApiItems,mapApiSource } from '../utils/api'
+import { ViewControlMain} from '../utils/control'
 
-export default function Entity() { 
+
+export default function Entity() {
     return (
-        <View style={{flexDirection:'column', flex: 1}}>
+        <View style={{}}>
 
-            {/* Title / breadcrumbs */}
-            <ViewProcessesTitle/>
+            {/* Show the entity focus (the primary record being viewed) */}
+            <ViewFocusMain/>
 
-            {/* Show the entity array (in whichever mode is selected, e.g. Calendar, Table etc.) */}
-            <View style={{flex:1,backgroundColor:'lightgray'}}>
-                <ViewDisplayMain/>
-            </View>
+            {/* Show the auxiliary entities (in whichever mode is selected, e.g. Calendar, Table etc.) */}
+            <ViewDisplayMain/>
 
             {/* Show links/tabs for Calendar, List, Table etc. */}
             <ViewDisplayTabs/>
 
+            {/* Placholder (Chris is working on this) - A panel for filtering/grouping/sorting */}
+            {/* <ViewControlMain/> */}
+
             {/* Show the actions tabs/links (e.g. add,edit,copy,delete,share etc.*/}
-            <ViewDisplayActions style={{flex:1}}/>
+            <ViewActionTabs/>
             
         </View>
     )

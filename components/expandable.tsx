9846aa8ea4f9f5565d936333b4fdf038c353acf7
Samine Dylah <samine@saminedylah.com>
2023-09-06 07:02:53 +0100

    entity display structure v4

--- components/expandable.tsx
+++ components/expandable.tsx
@@ -1,67 +1,77 @@
-import { Text, View } from "react-native";
-import { useLocation, Link } from "react-router-dom";
-import { newData as data } from "../../utils/static";
+import { useState } from "react";
+import { View, Pressable, Text } from "react-native";
+import { newData as data } from "../utils/static";
+import { ViewIconMain } from "../utils/icon";
+import { Link } from "react-router-dom";
 import React from "react";
 
-export const ViewProcessesTitle = () => {
-  // At the moment, this shows breadcrumbs for categories (e.g. governance > model > plan) from static.js
-  // But it will eventually be able to display a titlebar / breadcrumb bar for any entity from the database.
-  const path = useLocation()?.pathname?.split("/");
-  const process = data?.find((x) => x.nickname === path[2]);
-  const subprocesses = process && data.filter((x) => x.parent === process.id);
-  const parent = data?.find((y) => y.id === process?.parent);
-  const grandparent = data?.find((z) => z.id === parent?.parent);
+export const Expandable = ({ item }: any) => {
+  const [expanded, expandedToggle] = useState(false);
   return (
-    <View style={{ flexDirection: "column" }}>
-      {/* Title of the current process (breadcrumbs) */}
-      <Text style={{ fontSize: 24, height: 30 }}>
-        {grandparent?.id && grandparent?.id > 9 && (
-          <>
-            {" "}
-            <Link to={"/entity/" + grandparent.nickname}>
-              {grandparent.display_singular}
-            </Link>
-            {" > "}
-          </>
-        )}
-        {parent?.id && parent?.id > 9 && (
-          <>
-            {" "}
-            <Link to={"/entity/" + parent.nickname}>
-              {parent.display_singular}
-            </Link>
-            {" > "}
-          </>
-        )}
-        {process?.display_singular}
-      </Text>
-    </View>
-  );
-};
-
-export const ViewProcessesTabs = () => {
-  // At the moment, this shows breadcrumbs for categories (e.g. governance > model > plan) from static.js
-  // But it will eventually be able to display a titlebar / breadcrumb bar for any entity from the database.
-  const path = useLocation()?.pathname?.split("/");
-  const process = data?.find((x) => x.nickname === path[2]);
-  const subprocesses = process && data.filter((x) => x.parent === process.id);
-  const parent = data?.find((y) => y.id === process?.parent);
-  const grandparent = data?.find((z) => z.id === parent?.parent);
-  return (
-    <View style={{ flexDirection: "column" }}>
-      {/* Tabs for each subprocess */}
-      <View style={{ flexDirection: "row" }}>
-        {subprocesses?.map((x, i) => (
-          <Link style={{ flex: 1 }} key={i} to={`/entity/` + x.nickname}>
-            <Text>{x.display_singular}</Text>
-          </Link>
-        ))}
+    <View
+      style={{
+        flexDirection: "column",
+        paddingTop: 22,
+        paddingLeft: 20,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Link
+          style={{
+            flex: 3,
+            // backgroundColor: 'blue'
+          }}
+          to={"entity/" + item.nickname}
+        >
+          <Text selectable={false}>{item.display_singular}</Text>
+        </Link>
+        <Pressable
+          style={{}}
+          onPress={() => expandedToggle((oldValue) => !oldValue)}
+          style={{
+            minHeight: 40,
+            flexDirection: "row",
+            flex: 1,
+            // backgroundColor: 'blue',
+          }}
+        >
+          <ViewIconMain
+            // padding={5}
+            // paddingTop={15}
+            name={expanded ? "caretup" : "caretdown"}
+            source={"AntDesign"}
+            color={"gray"}
+            // size={10}
+          />
+        </Pressable>
       </View>
 
-      {/* Info on the current process (move this into pods*/}
-      {/* <Text style={{fontSize:12, height: 200, backgroundColor:'lightgray',overflow:'scroll'}}> */}
-      {/* {JSON.stringify({division,department,process},null,2)} */}
-      {/* </Text> */}
+      {expanded && (
+        <View
+          style={{
+            // flexDirection: 'column',
+            flex: 1,
+            flexGrow: 1,
+            minHeight: 200,
+            // backgroundColor: 'orange',
+          }}
+        >
+          {data
+            .filter(
+              (x) =>
+                (x.status === "3. Active" || __DEV__) && x.parent === item.id
+            )
+            .map((x, i) => (
+              <Link to={"entity/" + x.nickname} key={i}>
+                <Text selectable={false}>{x.display_singular}</Text>
+              </Link>
+            ))}
+        </View>
+      )}
     </View>
   );
 };

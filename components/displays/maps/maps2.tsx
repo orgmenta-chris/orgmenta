f0dfa651716249c8ce649b6999b15e2645a4a263
Freedom Loisa <kitakayaloisa@gmail.com>
2023-09-07 06:29:30 +0300

    reusable map component

--- components/displays/maps/maps2.tsx
+++ components/displays/maps/maps2.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Map, Marker, GeoJson } from "pigeon-maps";
-import worlMap from "../../../assets/custom.geo.json";
 
 const PegionMaps = (props: any) => {
   const { customerAddress } = props; // this data could be geocoded into lat and long coordinates for them to be rendered on the map
 
-  const geojson = worlMap;
+  const [worldMapJSON, setWorldMapJSON] = useState(null);
+
+  const fetchWorldMap = async () => {
+    try {
+      const response = await fetch(
+        "https://ik.imagekit.io/vg0ett8n6/custom.geo.json?updatedAt=1693968485445"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setWorldMapJSON(data);
+    } catch (error) {
+      console.error("Error fetching world map data:", error);
+    }
+  };
+
+  // this function essentially converts the user addresses into coordinates that can be used to render markers on the map.
+  const goeCodeAddresses = () => {
+    // todo
+  };
+
+  useEffect(() => {
+    fetchWorldMap();
+  }, []);
 
   return (
     <Map height={600} defaultZoom={2.5}>
       <GeoJson
-        data={geojson}
+        data={worldMapJSON}
         styleCallback={(feature: any, hover: any) => {
           if (feature.geometry.type === "LineString") {
             return { strokeWidth: "1", stroke: "black" };

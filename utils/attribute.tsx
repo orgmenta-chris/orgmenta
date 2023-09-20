0299d1e5daf4290345ee8b22888328a78da1f895
Samine Dylah <samine@saminedylah.com>
2023-09-20 14:26:39 +0100

    attribute tidy

--- utils/attribute.tsx
+++ utils/attribute.tsx
@@ -1,13 +1,10 @@
-import React, { memo } from 'react';
-import { createUuid4, typeUuid4, validateUuid4 } from './uuid' // note that we generate the id for tables here on the client / edge side (not the cloud db side), so that we can make immediate/optimistic changes to the ui & cache.
+//  An 'Attribute' is a property that an entity has.
+
+import { useState, memo, useEffect, useReducer} from 'react'
 import { instanceSupabaseClient, handleSupabaseResponse } from './supabase'
 import { useQuery, useMutation } from '@tanstack/react-query'
-import { TextInput,ScrollView, View, Text, Pressable } from 'react-native';
-import { Link, useLocation, Route, Routes } from 'react-router-dom'
-import { ViewListMain } from './list'
-import { ViewTableMain, useTableColumns } from '../components/displays/table/table'
-import { ViewJsonMain } from './json'
-import { useState, useMemo, useEffect, useReducer} from 'react'
+import { ScrollView, View, Text } from 'react-native';
+import { useTableColumns } from '../components/displays/table/table'
 import {
     createColumnHelper,
     flexRender,
@@ -36,58 +33,44 @@ export const useAttributeCreate = (props:any) => {
 
 // Main
 
-export const useAttributeMain = ({}:any)=> {
-    const query = useQuery({
-        queryKey:['attributes','main','all'],
-        queryFn:()=>{
-            return instanceSupabaseClient
-                .from("attributes")
-                .select()
-                .then(response=>response.data)
-        },
-        enabled: true
-    });
-    return query
+export const useAttributeMain = ({}: any) => { // get all of the attributes
+    const queryKey: (string | number)[] = ['attributes','main','all'];
+    const queryFn = async () => {
+      const response = await instanceSupabaseClient
+        .from("attributes")
+        .select()
+      return response.data;
+    };
+    const query = useQuery<any, Error>(queryKey, queryFn, { enabled: true });
+    return query;
 }
 
-// Chris is going to enhance this placeholder component
-export const ViewAttributeMain = memo(() => {
+export const ViewAttributeMain = memo(() => { // Chris is going to enhance this placeholder component
     const array = useAttributeMain({})
     const attributeColumnNames = ["id","status","a_name_singular","a_name_plural","a_display_singular","a_display_plural","created_at","updated_at","b_name_singular","b_name_plural","b_display_singular","b_display_plural","updated_by","a_cell_field","a_filter_field","b_cell_field","b_filter_field","created_by","a_table_sort","b_table_sort","a_filter_sort","b_filter_sort","a_table_width","b_table_width","a_filter_width","b_filter_width","a_form_field","a_form_width","a_form_sort","b_form_field","b_form_width","b_form_sort","a_options","b_options","a_description","b_description","a_options_min","b_options_min","a_options_max","b_options_max","a_options_default","b_options_default","a_templates","b_templates"];
     const columns = useTableColumns(attributeColumnNames);
     return (<>
         <Text>ViewAttributeMain</Text>
         <ViewAttributeTable data={array.data} columns={columns}/>
-        {/* Testing */}
-        {/* <View>
-            {array?.data?.map((x,i)=>
-                <View key={i} style={{backgroundColor:'lightblue',margin:10}}>
-                    <Text>{JSON.stringify(x,null,2)}</Text>
-                    <Text>{JSON.stringify(x,null,2)}</Text>
-                </View>
-            )}
-        </View> */}
    </>)
 });
 
 
 // Unioned
 
-export const useAttributeUnioned = (class_array=[] as any)=> {
+export const useAttributeUnioned = (class_array=[] as any) => { // get all of the attributes from both the A and B sides of attributes
   const searchArray = ["All"].concat(class_array).join(',') // make this dynamic and accept props
-  const query = useQuery({
-      queryKey:['attributes','unioned',searchArray],
-      queryFn:()=>{
-          return instanceSupabaseClient
-              .from("attributes_unioned")
-              .select()
-              .or(`class.cd.{${searchArray}}`) // match at least one value from the search array (or if null, assume that it is a universal attribute )
-              // .or(`class.is.null, class.cd.{${searchArray}}`) // match at least one value from the search array (or if null, assume that it is a universal attribute )
-              .then(response=>response.data)
-      },
-      enabled: true
-  });
-  return query
+  const queryKey: (string | number)[] = ['attributes','unioned',searchArray],
+  const queryFn = async () => {
+    const response = await instanceSupabaseClient
+    .from("attributes_unioned")
+    .select()
+    .or(`class.cd.{${searchArray}}`) // match at least one value from the search array (or if null, assume that it is a universal attribute )
+    // .or(`class.is.null, class.cd.{${searchArray}}`) // match at least one value from the search array (or if null, assume that it is a universal attribute )
+    return response.data;
+  };
+  const query = useQuery<any, Error>(queryKey, queryFn, { enabled: true });
+  return query;
 }
 
 export const ViewAttributeUnioned = memo(() => {
@@ -100,9 +83,11 @@ export const ViewAttributeUnioned = memo(() => {
     </>)
 });
 
+
+// Table
+
 export const ViewAttributeTable = ({...Input}) => {
     const columns = Input.columns;
-  
     const [columnResizeMode, setColumnResizeMode] = useState<ColumnResizeMode>('onChange')
 
     // When data is provided, set the data to state

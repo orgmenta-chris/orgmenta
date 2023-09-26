312c45b2dbd86d562529bf4f0a544ab70bb8ba5b
Freedom Loisa <kitakayaloisa@gmail.com>
2023-09-26 17:18:04 +0300

    can print to file html strings and be sharable - NOTE: works fine on mobil, need work around for web

--- utils/action.tsx
+++ utils/action.tsx
@@ -13,8 +13,10 @@ import { arrayStatusMain } from "./status";
 import { createUuid4 } from "./uuid";
 
 import ViewIconMain from "../components/displays/icons/ViewIconMain";
-import { TextInput, View, Text, Pressable } from "react-native";
+import { TextInput, View, Text, Pressable, Button } from "react-native";
 import { useState } from "react";
+import * as Print from "expo-print";
+import { shareAsync } from "expo-sharing";
 
 // Modal
 
@@ -308,10 +310,34 @@ export const ViewActionLink = ({}: any) => {
 // PDF
 
 export const ViewActionPDF = ({}: any) => {
+  const html = `
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+      </head>
+      <body style="text-align: center;">
+        <h1 style="font-size: 50px; font-family: Helvetica Neue; font-weight: normal;">
+          Hello Expo!
+        </h1>
+        <img
+          src="https://d30j33t1r58ioz.cloudfront.net/static/guides/sdk.png"
+          style="width: 90vw;" />
+      </body>
+    </html>
+  `;
+
+  const printToFile = async () => {
+    // On iOS/android prints the given html. On web prints the HTML from the current page.
+    const { uri } = await Print.printToFileAsync({ html });
+    // console.log("File has been saved to:", uri);
+    await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
+  };
+
   return (
     <View style={{ flexDirection: "column" }}>
       <Text>PDF</Text>
       <Text>render/display and allow for downloading of PDF files</Text>
+      <Button title="Print to PDF file" onPress={printToFile} />
     </View>
   );
 };

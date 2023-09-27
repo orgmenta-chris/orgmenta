8fc0fe0cb4f0b2792c3446609c025ea76a04a81d
Freedom Loisa <kitakayaloisa@gmail.com>
2023-09-27 11:37:40 +0300

    returned the maps component

--- utils/display.tsx
+++ utils/display.tsx
@@ -16,7 +16,7 @@ import {
 // import { ViewJsonMain } from "./json";
 // import { ViewIconMain } from "./icon";
 
-import { memo, useMemo, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import { View, Text, TouchableOpacity, Modal } from "react-native";
 // import MapChart from "../components/displays/maps";
 // import ViewDisplayCalendar from "../components/displays/calendar/ViewDisplayCalendar";
@@ -28,6 +28,7 @@ import { View, Text, TouchableOpacity, Modal } from "react-native";
 // import ViewJsonMain from "../components/displays/json/ViewJsonMain";
 import Timeline from "react-native-timeline-flatlist";
 import { Calendar } from "react-native-calendars";
+import { Map, Marker, GeoJson } from "pigeon-maps";
 
 // Dynamic
 
@@ -239,10 +240,54 @@ export const ViewDisplayTimeline = (props: any) => {
 // Maps
 
 export const ViewDisplayMaps = (props: any) => {
+  const { customerAddress } = props; // this data could be geocoded into lat and long coordinates for them to be rendered on the map
+
+  const [worldMapJSON, setWorldMapJSON] = useState(null);
+
+  const fetchWorldMap = async () => {
+    try {
+      const response = await fetch(
+        "https://ik.imagekit.io/vg0ett8n6/custom.geo.json?updatedAt=1693968485445"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setWorldMapJSON(data);
+    } catch (error) {
+      console.error("Error fetching world map data:", error);
+    }
+  };
+
+  // this function essentially converts the user addresses into coordinates that can be used to render markers on the map.
+  const goeCodeAddresses = () => {
+    // todo
+  };
+
+  useEffect(() => {
+    fetchWorldMap();
+  }, []);
+  
   return (
     <View style={{ maxHeight: 400 }}>
-      {/* <MapChart /> */}
-      {/* todo */}
+      <Map height={600} defaultZoom={2.5}>
+        <GeoJson
+          data={worldMapJSON}
+          styleCallback={(feature: any, hover: any) => {
+            if (feature.geometry.type === "LineString") {
+              return { strokeWidth: "1", stroke: "black" };
+            }
+            return {
+              fill: "#d4e6ec99",
+              strokeWidth: "1",
+              stroke: "white",
+              r: "20",
+            };
+          }}
+        />
+        <Marker width={50} anchor={[13.75396, 100.50224]} color="gray" />
+        <Marker width={50} anchor={[-1.292066, 36.821945]} color="gray" />
+      </Map>  
     </View>
   );
 };

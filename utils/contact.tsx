b1a7074e1904fb13038fbec64e4be118134fe5ec
Samine Dylah <samine@saminedylah.com>
2023-09-28 08:09:24 +0100

    inquiry table created, inquiry form tested and errors, success message component added, home page set to show inquiry form if !__DEV__

--- utils/contact.tsx
+++ utils/contact.tsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
 import { useMutation } from "@tanstack/react-query";
 import { instanceSupabaseClient, handleSupabaseResponse } from "./supabase";
-import { createUuid4 } from "./uuid";
+
+// Main
+
+interface TypeInquiryMain {
+  name?: string;
+  email?: string;
+  message?: string;
+}
 
 export const ViewInquiryMain = () => {
-  const [state,set] = useState({})
+  const [state, set] = useState<TypeInquiryMain>({});
   const create = useInquiryCreate(state);
   return (
     <ScrollView style={{ flexDirection: "column" }}>
@@ -13,13 +20,15 @@ export const ViewInquiryMain = () => {
       <View style={{ flexDirection: "row" }}>
         <Text style={{height: 40, textAlignVertical:'center', flex: 1}}>Name: </Text>
         <TextInput 
+          value={state.name || ''}
           style={{height: 40, textAlignVertical:'center', flex: 3, borderWidth:1}}
           onChangeText={(newData)=>set({...state,name:newData})}
         />
       </View>
       <View style={{ flexDirection: "row" }}>
         <Text style={{height: 40, textAlignVertical:'center', flex: 1}}>Email*: </Text>
-        <TextInput 
+        <TextInput
+          value={state.email || ''} 
           style={{height: 40, textAlignVertical:'center', flex: 3, borderWidth:1}}
           onChangeText={(newData)=>set({...state,email:newData})}
         />
@@ -27,6 +36,7 @@ export const ViewInquiryMain = () => {
       <View style={{ flexDirection: "row" }}>
         <Text style={{height: 40, textAlignVertical:'center', flex: 1}}>Message*: </Text>
         <TextInput 
+          value={state.message || ''}
           style={{height: 40, textAlignVertical:'center', flex: 3, borderWidth:1}}
           onChangeText={(newData)=>set({...state,message:newData})}
         />
@@ -36,15 +46,18 @@ export const ViewInquiryMain = () => {
           style={{ backgroundColor: (state?.name && state?.message ) ? "lightblue" : "gray" }}
           onPress={() => {
             create.mutate();
-            set((old) => ({ ...old, id: createUuid4() }));
+            set((old) => ({}));
           }}
         >
         <Text>Submit</Text>
       </Pressable>
       <View style={{ flexDirection: "row" }}>
         <Text style={{textAlign:'center', textAlignVertical:'center', flex: 1}}>
-          {/* (status of submission here) */}
-          <Text>{JSON.stringify(create, null, 2)}</Text>
+          <Text>
+            {create.isLoading && `Submitting...`}
+            {create.isError && `Error: ${JSON.stringify(create.error,null,2)}`}
+            {create.isSuccess && `Submitted. Thank you, we will respond ASAP.`}
+          </Text>
         </Text>
       </View>
     </ScrollView>
@@ -55,11 +68,10 @@ export const ViewInquiryMain = () => {
 // Create
 
 export async function requestInquiryCreate(data: any) {
-  console.log('data',data)
-  // return await instanceSupabaseClient
-  //   .from("attributes")
-  //   .insert(attribute)
-  //   .then(handleSupabaseResponse as any);
+  return await instanceSupabaseClient
+    .from("inquiries")
+    .insert(data)
+    .then(handleSupabaseResponse as any);
 }
 
 export const useInquiryCreate = (data: any) => {

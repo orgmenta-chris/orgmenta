95a50673a506d1abdac5d0542679365ba4f5775d
Freedom Loisa <kitakayaloisa@gmail.com>
2023-09-28 05:31:52 +0300

    proof of concept calendar component

--- utils/display.tsx
+++ utils/display.tsx
@@ -17,7 +17,14 @@ import {
 // import { ViewIconMain } from "./icon";
 
 import { ReactElement, memo, useEffect, useMemo, useState } from "react";
-import { View, Text, TouchableOpacity, Modal, ViewStyle } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  ViewStyle,
+  Button,
+} from "react-native";
 // import MapChart from "../components/displays/maps";
 // import ViewDisplayCalendar from "../components/displays/calendar/ViewDisplayCalendar";
 // import ViewDisplayForm from "../components/displays/forms/ViewDisplayForm";
@@ -132,12 +139,8 @@ export const ViewDisplayTable = (props: any) => {
 // Calendar
 
 export const ViewDisplayCalendar = (props: any) => {
-  const today = new Date(); // Get the current date and time
-  const tomorrow = new Date(today); // Create a copy of the current date
-  tomorrow.setDate(today.getDate() + 1); // Set the copy to tomorrow
-
   interface CalendarProps<T extends ICalendarEventBase> {
-    events: T;
+    events: T[];
     height: number;
     overlapOffset?: number;
     hourRowHeight?: number;
@@ -164,7 +167,7 @@ export const ViewDisplayCalendar = (props: any) => {
     onChangeDate?: DateRangeHandler;
     onPressCell?: (date: Date) => void;
     onPressDateHeader?: (date: Date) => void;
-    onPressEvent?: (event: any) => void;
+    onPressEvent?: (event: T) => void;
     eventMinHeightForMonthView?: number;
     activeDate?: Date;
     moreLabel?: string;
@@ -172,36 +175,185 @@ export const ViewDisplayCalendar = (props: any) => {
     sortedMonthView?: boolean;
   }
 
-  const events = [
+  interface ICalendarEvent extends ICalendarEventBase {}
+
+  const [mode, setMode] = useState("month");
+
+  const events: ICalendarEvent[] = [
     {
-      title: "Meeting today",
-      start: today, // Use the current date for the start time
-      end: new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate(),
-        11,
-        0
-      ), // Set the end time to 11:00 AM today
-      hideNowIndicator: false,
+      title: "Meeting 1",
+      start: new Date(2023, 8, 1, 10, 0), // September 1st, 10:00 AM
+      end: new Date(2023, 8, 1, 11, 0), // September 1st, 11:00 AM
     },
     {
-      title: "Coffee break tomorrow",
-      start: tomorrow, // Use tomorrow's date for the start time
-      end: new Date(
-        tomorrow.getFullYear(),
-        tomorrow.getMonth(),
-        tomorrow.getDate(),
-        15,
-        30
-      ), // Set the end time to 3:30 PM tomorrow
-      hideNowIndicator: false,
+      title: "Conference",
+      start: new Date(2023, 8, 5, 14, 30), // September 5th, 2:30 PM
+      end: new Date(2023, 8, 5, 17, 0), // September 5th, 5:00 PM
+    },
+    {
+      title: "Lunch",
+      start: new Date(2023, 8, 10, 12, 0), // September 10th, 12:00 PM
+      end: new Date(2023, 8, 10, 13, 0), // September 10th, 1:00 PM
+    },
+    {
+      title: "Team Meeting",
+      start: new Date(2023, 8, 15, 9, 30), // September 15th, 9:30 AM
+      end: new Date(2023, 8, 15, 10, 30), // September 15th, 10:30 AM
+    },
+    {
+      title: "Workshop",
+      start: new Date(2023, 8, 20, 14, 0), // September 20th, 2:00 PM
+      end: new Date(2023, 8, 20, 17, 0), // September 20th, 5:00 PM
+    },
+    {
+      title: "Coffee Break",
+      start: new Date(2023, 8, 25, 15, 0), // September 25th, 3:00 PM
+      end: new Date(2023, 8, 25, 15, 30), // September 25th, 3:30 PM
+    },
+    {
+      title: "Training",
+      start: new Date(2023, 8, 3, 9, 0), // September 3rd, 9:00 AM
+      end: new Date(2023, 8, 3, 12, 0), // September 3rd, 12:00 PM
+    },
+    {
+      title: "Project Review",
+      start: new Date(2023, 8, 8, 13, 0), // September 8th, 1:00 PM
+      end: new Date(2023, 8, 8, 14, 30), // September 8th, 2:30 PM
+    },
+    {
+      title: "Client Meeting",
+      start: new Date(2023, 8, 12, 11, 0), // September 12th, 11:00 AM
+      end: new Date(2023, 8, 12, 12, 0), // September 12th, 12:00 PM
+    },
+    {
+      title: "Team Building",
+      start: new Date(2023, 8, 17, 10, 0), // September 17th, 10:00 AM
+      end: new Date(2023, 8, 17, 16, 0), // September 17th, 4:00 PM
+    },
+    {
+      title: "Lunch Meeting",
+      start: new Date(2023, 8, 22, 12, 30), // September 22nd, 12:30 PM
+      end: new Date(2023, 8, 22, 13, 30), // September 22nd, 1:30 PM
+    },
+    {
+      title: "Training Session",
+      start: new Date(2023, 8, 6, 15, 0), // September 6th, 3:00 PM
+      end: new Date(2023, 8, 6, 17, 0), // September 6th, 5:00 PM
+    },
+    {
+      title: "Client Presentation",
+      start: new Date(2023, 8, 13, 14, 0), // September 13th, 2:00 PM
+      end: new Date(2023, 8, 13, 16, 0), // September 13th, 4:00 PM
+    },
+    {
+      title: "Project Kickoff",
+      start: new Date(2023, 8, 27, 9, 0), // September 27th, 9:00 AM
+      end: new Date(2023, 8, 27, 10, 0), // September 27th, 10:00 AM
+    },
+    {
+      title: "Team Lunch",
+      start: new Date(2023, 8, 30, 12, 0), // September 30th, 12:00 PM
+      end: new Date(2023, 8, 30, 13, 0), // September 30th, 1:00 PM
     },
   ];
 
+  const calendarProps: CalendarProps<ICalendarEventBase> = {
+    events,
+    height: 600,
+    overlapOffset: 10, // Set overlap offset to 10 pixels
+    hourRowHeight: 60, // Set hour row height to 60 pixels
+    ampm: true, // Display time in AM/PM format
+    // date: new Date(2023, 8, 1), // Set the initial date to September 1st, 2023
+    eventCellStyle: () => {
+      // Custom event cell style function
+      return {
+        backgroundColor: "cyan",
+        borderColor: "black",
+      };
+    },
+    calendarContainerStyle: {
+      // Custom styles for the calendar container
+      backgroundColor: "lightgray",
+    },
+    headerContainerStyle: {
+      // Custom styles for the header container
+      backgroundColor: "white",
+    },
+    bodyContainerStyle: {
+      // Custom styles for the body container
+      backgroundColor: "white",
+    },
+    renderEvent: (event, touchableOpacityProps) => {
+      // Custom render for events
+      return (
+        <TouchableOpacity {...touchableOpacityProps}>
+          <Text>{event.title}</Text>
+          <Text>{`${event.start.toLocaleTimeString()} - ${event.end.toLocaleTimeString()}`}</Text>
+        </TouchableOpacity>
+      );
+    },
+    // renderHeader: (props) => {
+    //   // Custom render for header
+    //   return <CalendarHeader {...props} />;
+    // },
+    // renderHeaderForMonthView: (props) => {
+    //   // Custom render for month view header
+    //   return <CalendarHeaderForMonthView {...props} />;
+    // },
+    locale: "en-US", // Set the locale to English (United States)
+    hideNowIndicator: false, // Show the "now" indicator
+    // @ts-ignore
+    mode: mode, // Set the initial mode to month view
+    scrollOffsetMinutes: 30, // Scroll calendar by 30 minutes
+    showTime: true, // Show time on events
+    swipeEnabled: true, // Enable swipe navigation
+    weekStartsOn: 0, // Start the week on Sunday
+    weekEndsOn: 6, // End the week on Saturday
+    onChangeDate: (dateRange) => {
+      // Handle date range change
+      console.log("Date range changed:", dateRange);
+    },
+    onPressCell: (date) => {
+      // Handle cell press
+      console.log("Cell pressed:", date);
+    },
+    onPressDateHeader: (date) => {
+      // Handle date header press
+      console.log("Date header pressed:", date);
+    },
+    onPressEvent: (event) => {
+      // Handle event click
+      console.log("Event clicked:", event);
+    },
+    eventMinHeightForMonthView: 40, // Set the minimum event height for month view
+    activeDate: new Date(), // Set the active date to September 15th, 2023
+    moreLabel: "More", // Customize the "more" label for overlapping events
+    showAdjacentMonths: true, // Show adjacent months in month view
+    sortedMonthView: false, // Disable sorted month view
+  };
+
   return (
-    <View style={{ maxHeight: 400 }}>
-      <Calendar events={events} height={600} />
+    <View style={{ minHeight: 400 }}>
+      <View style={{ marginVertical: 10 }}>
+        {
+          // @ts-ignore
+          <Calendar {...calendarProps} />
+        }
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: 5,
+          marginBottom: 10,
+        }}
+      >
+        <Button onPress={() => setMode("month")} title="Month" />
+        <Button onPress={() => setMode("week")} title="Week" />
+        <Button onPress={() => setMode("3days")} title="3 Days" />
+        <Button onPress={() => setMode("day")} title="Day" />
+      </View>
     </View>
   );
 };

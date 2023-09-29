67ad20fc10ca49df40639a651fe4096de7b4e728
Samine Dylah <samine@saminedylah.com>
2023-09-29 15:18:44 +0100

    workflow jsx to tsx & placeholder component

--- utils/workflow.jsx
+++
@@ -1,3 +0,0 @@
-// Placeholder
-// A 'workflow' will refer to an automatic procedure / rule being run
-// E.g. the client_administrator may want to automatically parse an email to see if the subject contains 'urgent', and automatically assign the ticket as a p1.
---
+++ utils/workflow.tsx
@@ -0,0 +1,15 @@
+// Placeholder
+// A 'workflow' will refer to an automatic procedure / rule being run
+// E.g. the client_administrator may want to automatically parse an email to see if the subject contains 'urgent', and automatically assign the ticket as a p1.
+
+import { Text, View } from "react-native";
+
+
+export const ViewWorkflowMain = ({ children }: any) => {
+  return (
+    <View>
+      <Text>ViewWorkflowMain placeholder</Text>
+      <Text>{children}</Text>
+    </View>
+  );
+};

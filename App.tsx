27432afc3473d993895fdcc8783dba74f7ff0e83
Freedom Loisa <kitakayaloisa@gmail.com>
2023-10-04 11:03:25 +0300

    modified App.tsx

--- App.tsx
+++ App.tsx
@@ -45,156 +45,185 @@ import { ViewOrgmentaModal } from "./utils/orgmenta";
 import { ViewBrowseModal } from "./utils/browse";
 import { ViewSpaceModal } from "./utils/space";
 import { ViewUserModal } from "./utils/user";
-import { usePlatformCssweb } from "./utils/platform";
+import { UtilityPlatformMain, usePlatformCssweb } from "./utils/platform";
+import { useEffect, useState } from "react";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import {
+  STAGING_STRIPE_PUBLISHABLE_KEY,
+  PRODUCTION_STRIPE_PUBLISHABLE_KEY,
+} from "@env";
 
 export default function App() {
   usePlatformCssweb(); // shim to add css to web
+
+  // const [publishableKey, setPublishableKey] = useState("");
+
+  // const fetchPublishableKey = async () => {
+  //   if (__DEV__) {
+  //     setPublishableKey(`${STAGING_STRIPE_PUBLISHABLE_KEY}`);
+  //   } else {
+  //     setPublishableKey(`${PRODUCTION_STRIPE_PUBLISHABLE_KEY}`);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchPublishableKey();
+  // }, []);
+
+  const AppBody = () => {
+    return (
+      <ViewRouterProvider>
+        <ViewQueryerProvider>
+          <ViewStatusbarMain />
+          <ViewWindowMain>
+            <ViewOrgmentaHeader />
+            <ViewRouterRoutes>
+              {/* Home Page */}
+              <ViewRouterRoute path="/" element={<Home />} />
+              {/* App Pages */}
+              <ViewRouterRoute
+                path="app/"
+                element={<ExecuteRouterNavigate to="product" replace />}
+              />
+              <ViewRouterRoute
+                path="app/product"
+                element={<ViewOrgmentaProduct />}
+              />
+              <ViewRouterRoute
+                path="app/pricing"
+                element={<ViewOrgmentaPricing />}
+              />
+              <ViewRouterRoute
+                path="app/roadmap"
+                element={<ViewOrgmentaRoadmap />}
+              />
+              <ViewRouterRoute
+                path="app/compare"
+                element={<ViewOrgmentaCompare />}
+              />
+              <ViewRouterRoute
+                path="app/industries"
+                element={<ViewOrgmentaIndustries />}
+              />
+              <ViewRouterRoute
+                path="app/frameworks"
+                element={<ViewOrgmentaFrameworks />}
+              />
+              {/* Company Pages */}
+              <ViewRouterRoute
+                path="company/"
+                element={<ExecuteRouterNavigate to="about" replace />}
+              />
+              <ViewRouterRoute
+                path="company/about"
+                element={<ViewOrgmentaAbout />}
+              />
+              <ViewRouterRoute
+                path="company/privacy"
+                element={<ViewOrgmentaPrivacy />}
+              />
+              <ViewRouterRoute
+                path="company/terms"
+                element={<ViewOrgmentaTerms />}
+              />
+              <ViewRouterRoute
+                path="company/socials"
+                element={<ViewOrgmentaSocials />}
+              />
+              <ViewRouterRoute
+                path="company/contact"
+                element={<ViewOrgmentaContact />}
+              />
+              <ViewRouterRoute
+                path="company/partner"
+                element={<ViewOrgmentaPartner />}
+              />
+              {/* Community Pages */}
+              <ViewRouterRoute
+                path="community/"
+                element={<ExecuteRouterNavigate to="forums" replace />}
+              />
+              <ViewRouterRoute
+                path="community/news"
+                element={<ViewOrgmentaNews />}
+              />
+              <ViewRouterRoute
+                path="community/forums"
+                element={<ViewOrgmentaForums />}
+              />
+              <ViewRouterRoute
+                path="community/guides"
+                element={<ViewOrgmentaGuides />}
+              />
+              <ViewRouterRoute
+                path="community/whitepapers"
+                element={<ViewOrgmentaWhitepapers />}
+              />
+              <ViewRouterRoute
+                path="community/enhancements"
+                element={<ViewOrgmentaEnhancements />}
+              />
+              {/* Entity Page */}
+              <ViewRouterRoute
+                path="entity/"
+                element={<ExecuteRouterNavigate to="all" replace />}
+              />
+              <ViewRouterRoute
+                path="entity/:entityid"
+                element={<ExecuteRouterNavigate to="pods" replace />}
+              />
+              <ViewRouterRoute
+                path="entity/:entityid/:mode/*"
+                element={<Entity />}
+              />
+              {/* User Page */}
+              <ViewRouterRoute
+                path="users/"
+                element={<ExecuteRouterNavigate to="all" replace />}
+              />
+              <ViewRouterRoute path="users/:userid/*" element={<User />} />
+              {/* Space Page */}
+              <ViewRouterRoute
+                path="spaces/"
+                element={<ExecuteRouterNavigate to="all" replace />}
+              />
+              <ViewRouterRoute path="spaces/:spaceid/*" element={<Space />} />
+              {/* Browse Page */}
+              <ViewRouterRoute
+                path="browse/"
+                element={<ExecuteRouterNavigate to="all/all" replace />}
+              />
+              <ViewRouterRoute
+                path="browse/:browsemode/:searchterm"
+                element={<Browse />}
+              />
+              {/* Attributes Page */}
+              <ViewRouterRoute
+                path="attributes/"
+                element={<ExecuteRouterNavigate to="all" replace />}
+              />
+              <ViewRouterRoute
+                path="attributes/:mode/:attributeid/*"
+                element={<Attribute />}
+              />
+            </ViewRouterRoutes>
+            {/* Modals */}
+            <ViewSpaceModal />
+            <ViewBookmarkModal />
+            <ViewOrgmentaModal />
+            <ViewBrowseModal />
+            <ViewUserModal />
+          </ViewWindowMain>
+        </ViewQueryerProvider>
+      </ViewRouterProvider>
+    );
+  };
+
   return (
     // <ViewMsalProvider instance={msalInstance}> */}
-    <ViewRouterProvider>
-      <ViewQueryerProvider>
-        <ViewStatusbarMain />
-        <ViewWindowMain>
-          <ViewOrgmentaHeader />
-          <ViewRouterRoutes>
-            {/* Home Page */}
-            <ViewRouterRoute path="/" element={<Home />} />
-            {/* App Pages */}
-            <ViewRouterRoute
-              path="app/"
-              element={<ExecuteRouterNavigate to="product" replace />}
-            />
-            <ViewRouterRoute
-              path="app/product"
-              element={<ViewOrgmentaProduct />}
-            />
-            <ViewRouterRoute
-              path="app/pricing"
-              element={<ViewOrgmentaPricing />}
-            />
-            <ViewRouterRoute
-              path="app/roadmap"
-              element={<ViewOrgmentaRoadmap />}
-            />
-            <ViewRouterRoute
-              path="app/compare"
-              element={<ViewOrgmentaCompare />}
-            />
-            <ViewRouterRoute
-              path="app/industries"
-              element={<ViewOrgmentaIndustries />}
-            />
-            <ViewRouterRoute
-              path="app/frameworks"
-              element={<ViewOrgmentaFrameworks />}
-            />
-            {/* Company Pages */}
-            <ViewRouterRoute
-              path="company/"
-              element={<ExecuteRouterNavigate to="about" replace />}
-            />
-            <ViewRouterRoute
-              path="company/about"
-              element={<ViewOrgmentaAbout />}
-            />
-            <ViewRouterRoute
-              path="company/privacy"
-              element={<ViewOrgmentaPrivacy />}
-            />
-            <ViewRouterRoute
-              path="company/terms"
-              element={<ViewOrgmentaTerms />}
-            />
-            <ViewRouterRoute
-              path="company/socials"
-              element={<ViewOrgmentaSocials />}
-            />
-            <ViewRouterRoute
-              path="company/contact"
-              element={<ViewOrgmentaContact />}
-            />
-            <ViewRouterRoute
-              path="company/partner"
-              element={<ViewOrgmentaPartner />}
-            />
-            {/* Community Pages */}
-            <ViewRouterRoute
-              path="community/"
-              element={<ExecuteRouterNavigate to="forums" replace />}
-            />
-            <ViewRouterRoute
-              path="community/news"
-              element={<ViewOrgmentaNews />}
-            />
-            <ViewRouterRoute
-              path="community/forums"
-              element={<ViewOrgmentaForums />}
-            />
-            <ViewRouterRoute
-              path="community/guides"
-              element={<ViewOrgmentaGuides />}
-            />
-            <ViewRouterRoute
-              path="community/whitepapers"
-              element={<ViewOrgmentaWhitepapers />}
-            />
-            <ViewRouterRoute
-              path="community/enhancements"
-              element={<ViewOrgmentaEnhancements />}
-            />
-            {/* Entity Page */}
-            <ViewRouterRoute
-              path="entity/"
-              element={<ExecuteRouterNavigate to="all" replace />}
-            />
-            <ViewRouterRoute
-              path="entity/:entityid"
-              element={<ExecuteRouterNavigate to="pods" replace />}
-            />
-            <ViewRouterRoute
-              path="entity/:entityid/:mode/*"
-              element={<Entity />}
-            />
-            {/* User Page */}
-            <ViewRouterRoute
-              path="users/"
-              element={<ExecuteRouterNavigate to="all" replace />}
-            />
-            <ViewRouterRoute path="users/:userid/*" element={<User />} />
-            {/* Space Page */}
-            <ViewRouterRoute
-              path="spaces/"
-              element={<ExecuteRouterNavigate to="all" replace />}
-            />
-            <ViewRouterRoute path="spaces/:spaceid/*" element={<Space />} />
-            {/* Browse Page */}
-            <ViewRouterRoute
-              path="browse/"
-              element={<ExecuteRouterNavigate to="all/all" replace />}
-            />
-            <ViewRouterRoute
-              path="browse/:browsemode/:searchterm"
-              element={<Browse />}
-            />
-            {/* Attributes Page */}
-            <ViewRouterRoute
-              path="attributes/"
-              element={<ExecuteRouterNavigate to="all" replace />}
-            />
-            <ViewRouterRoute
-              path="attributes/:mode/:attributeid/*"
-              element={<Attribute />}
-            />
-          </ViewRouterRoutes>
-          {/* Modals */}
-          <ViewSpaceModal />
-          <ViewBookmarkModal />
-          <ViewOrgmentaModal />
-          <ViewBrowseModal />
-          <ViewUserModal />
-        </ViewWindowMain>
-      </ViewQueryerProvider>
-    </ViewRouterProvider>
+    // <StripeProvider publishableKey={publishableKey}>
+    <AppBody />
+    //   </StripeProvider>
     // </ViewMsalProvider> */}
   );
 }

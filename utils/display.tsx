075a1c2dd7dcbd132a74744ef493430604045cec
Freedom Loisa <kitakayaloisa@gmail.com>
2023-10-04 17:27:18 +0300

    added JSON display - collapsable/expandable json tree list view

--- utils/display.tsx
+++ utils/display.tsx
@@ -46,6 +46,8 @@ import {
   Mode,
   WeekNum,
 } from "react-native-big-calendar";
+import JSONTree from "react-native-json-tree";
+// import { Map as ImmutableMap } from "immutable";
 
 // Dynamic
 
@@ -434,15 +436,88 @@ export const ViewDisplayMaps = (props: any) => {
 // Json
 
 export const ViewDisplayJson = (props: any) => {
-  const schema = props.schema;
-  const auxiliary = props.auxiliary;
-  // const columns = useTableColumns(
-  //   schema.data?.map((x: any) => x.focus_columns.name_singular)
-  // );
+  const jsonData = {
+    name: "John Doe",
+    age: 30,
+    address: {
+      street: "123 Main St",
+      city: "New York",
+      zipCode: "10001",
+    },
+    email: "john.doe@example.com",
+    isEmployed: true,
+    hobbies: ["Reading", "Hiking"],
+    pets: [
+      {
+        name: "Fido",
+        species: "Dog",
+        age: 5,
+      },
+      {
+        name: "Whiskers",
+        species: "Cat",
+        age: 3,
+      },
+    ],
+    education: [
+      {
+        degree: "Bachelor's",
+        major: "Computer Science",
+        university: "Example University",
+        year: 2015,
+      },
+      {
+        degree: "Master's",
+        major: "Business Administration",
+        university: "Another University",
+        year: 2018,
+      },
+    ],
+    // immutable: ImmutableMap({ key: "value" }),
+  };
+
+  const theme = {
+    scheme: "monokai",
+    author: "wimer hazenberg (http://www.monokai.nl)",
+    base00: "#272822",
+    base01: "#383830",
+    base02: "#49483e",
+    base03: "#75715e",
+    base04: "#a59f85",
+    base05: "#f8f8f2",
+    base06: "#f5f4f1",
+    base07: "#f9f8f5",
+    base08: "#f92672",
+    base09: "#fd971f",
+    base0A: "#f4bf75",
+    base0B: "#a6e22e",
+    base0C: "#a1efe4",
+    base0D: "#66d9ef",
+    base0E: "#ae81ff",
+    base0F: "#cc6633",
+  };
+
   return (
     <View style={{ flexDirection: "column" }}>
-      {/* <ViewJsonMain schema={schema} auxiliary={auxiliary} columns={columns} /> */}
-      {/* todo */}
+      <JSONTree
+        data={jsonData}
+        theme={{
+          extend: theme,
+          // underline keys for literal values
+          valueLabel: {
+            textDecoration: "underline",
+          },
+          // switch key for objects to uppercase when object is expanded.
+          // `nestedNodeLabel` receives additional arguments `expanded` and `keyPath`
+          nestedNodeLabel: ({ style }, nodeType, expanded) => ({
+            style: {
+              ...style,
+              textTransform: expanded ? "uppercase" : style.textTransform,
+            },
+          }),
+        }}
+        invertTheme={true}
+      />
     </View>
   );
 };

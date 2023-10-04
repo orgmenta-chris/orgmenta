9ee8244c173aabb4ff4e41fd11f40502ea697bae
Freedom Loisa <kitakayaloisa@gmail.com>
2023-10-04 11:06:19 +0300

    leveraged the power of env variables

--- api/authConfig.tsx
+++ api/authConfig.tsx
@@ -1,9 +1,9 @@
 import { Configuration, PublicClientApplication } from "@azure/msal-browser";
 import {
-  STAGING_CLIENT_ID,
-  STAGING_TENANT_ID,
-  PRODUCTION_CLIENT_ID,
-  PRODUCTION_TENANT_ID,
+  STAGING_AZURE_CLIENT_ID,
+  STAGING_AZURE_TENANT_ID,
+  PRODUCTION_AZURE_CLIENT_ID,
+  PRODUCTION_AZURE_TENANT_ID,
 } from "@env";
 
 // Microsoft Authentication Library configuration
@@ -13,12 +13,12 @@ let tenantIdVar: any;
 
 if (__DEV__) {
   // You are in development mode
-  clientIdVar = `${STAGING_CLIENT_ID}`;
-  tenantIdVar = `${STAGING_TENANT_ID}`;
+  clientIdVar = `${STAGING_AZURE_CLIENT_ID}`;
+  tenantIdVar = `${STAGING_AZURE_TENANT_ID}`;
 } else {
   // You are in production mode
-  clientIdVar = `${PRODUCTION_CLIENT_ID}`;
-  tenantIdVar = `${PRODUCTION_TENANT_ID}`;
+  clientIdVar = `${PRODUCTION_AZURE_CLIENT_ID}`;
+  tenantIdVar = `${PRODUCTION_AZURE_TENANT_ID}`;
 }
 
 const msalConfig: Configuration = {

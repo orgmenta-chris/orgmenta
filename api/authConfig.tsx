5b314a7bb2b6c1761194a2c3a75da4ecff22be51
Freedom Loisa <kitakayaloisa@gmail.com>
2023-10-18 15:27:54 +0300

    updated code base with new msal implementation

--- api/authConfig.tsx
+++ api/authConfig.tsx
@@ -1,8 +1,13 @@
-import { Configuration, PublicClientApplication } from "@azure/msal-browser";
-import {
-  STAGING_AZURE_CLIENT_ID,
-  STAGING_AZURE_TENANT_ID,
-} from "@env";
+// import { Configuration, PublicClientApplication } from "@azure/msal-browser";
+import PublicClientApplication from "react-native-msal";
+import type {
+  MSALConfiguration /*, etc */,
+  MSALSilentParams,
+} from "react-native-msal";
+// @ts-ignore
+import { STAGING_AZURE_CLIENT_ID, STAGING_AZURE_TENANT_ID } from "@env";
+import { result } from "underscore";
+import useTokenStore from "../states/api/storeToken";
 import { requestVaultItem } from "../utils/vault";
 
 // Microsoft Authentication Library configuration
@@ -20,29 +25,36 @@ if (__DEV__) {
   tenantIdVar = requestVaultItem("PRODUCTION_AZURE_TENANT_ID");
 }
 
-const msalConfig: Configuration = {
+// const getToken = useTokenStore((state: any) => state.token);
+
+export const scopes = [
+  "User.ReadWrite.All",
+  "Mail.ReadWrite",
+  "Mail.ReadWrite.Shared",
+  "Calendars.ReadWrite",
+  "Calendars.ReadWrite.Shared",
+];
+
+const msalConfig: MSALConfiguration = {
   auth: {
     clientId: `${clientIdVar}`,
     authority: `https://login.microsoftonline.com/${tenantIdVar}`,
     redirectUri: "/",
-    postLogoutRedirectUri: "/",
   },
 };
 
-export const loginRequest = {
-  scopes: [
-    "User.ReadWrite.All",
-    "Mail.ReadWrite",
-    "Mail.ReadWrite.Shared",
-    "Calendars.ReadWrite",
-    "Calendars.ReadWrite.Shared",
-  ],
-};
+// export const msalSilentConfig: MSALSilentParams = {
+//   account: getToken!.account || "", // or get this by filtering the result from `pca.getAccounts` (see below)
+//   scopes,
+//   forceRefresh: true,
+// };
 
 export const url = "https://graph.microsoft.com/v1.0";
 
 // Public Client Application Instance
 
-export const msalInstance = new PublicClientApplication(msalConfig);
+// export const msalInstance = new PublicClientApplication(msalConfig);
+
+export const publicClientApplication = new PublicClientApplication(msalConfig);
 
 // Get Access Token within a react component refer to accessToken.tsx

fa7ee38409007ee43356e5fc570391b4d2bd8d03
orgmenta-chris <chris@orgmenta.com>
2023-10-19 06:42:21 +0100

    type 'all' button

--- utils/type.tsx
+++ utils/type.tsx
@@ -6,18 +6,21 @@ import { ViewIconMain } from "./icon";
 import { useRouterLocation, ViewRouterLinkthemed } from "./router";
 import { ViewTypographyText } from "./typography";
 
-
 export const arrayTypeMain = [
-  {title:"Area",iconName: 'category', iconSource: 'MaterialIcons'}, //A 1  OR GROUP? this would possibly be better for implying 'categories' as well as 'areas' BUT would conflict with Group in the context of grouping, filtering etc.. (And, would allow us to move 'contact groups' in here... but is that a good thing? )
-  {title:"Contact",iconName: 'contacts', iconSource: 'AntDesign'}, //C 2
-  {title:"Message",iconName: 'chat', iconSource: 'Entypo'}, //M 3
-  {title:"Task",iconName: 'tasks', iconSource: 'FontAwesome5'}, //T 4
-  {title:"Event",iconName: 'event', iconSource: 'MaterialIcons'}, //E 5
-  {title:"Item",iconName: 'box', iconSource: 'Feather'}, //I 6
-  {title:"Reference",iconName: 'paperclip', iconSource: 'Feather'}, //R 7
-  {title:"Place",iconName: 'location', iconSource: 'Entypo'}, //P 8 //was Location but L was used by Log. 
-  {title:"Blueprint",iconName: 'flash', iconSource: 'FontAwesome'},//B 9 //was template but T conflict (with 'template' and 'rule' as sub types/classes)? - this would actually mean we had a different starting character for each type (e.g. B for Blueprints) which could help with shortcuts.
-  {title:"Log",iconName: 'timeline-clock', iconSource: 'MaterialCommunityIcons'}, //L 10(was conflicting with Location but ok now.)
+  { title: "Area", iconName: "category", iconSource: "MaterialIcons" }, //A 1  OR GROUP? this would possibly be better for implying 'categories' as well as 'areas' BUT would conflict with Group in the context of grouping, filtering etc.. (And, would allow us to move 'contact groups' in here... but is that a good thing? )
+  { title: "Contact", iconName: "contacts", iconSource: "AntDesign" }, //C 2
+  { title: "Message", iconName: "chat", iconSource: "Entypo" }, //M 3
+  { title: "Task", iconName: "tasks", iconSource: "FontAwesome5" }, //T 4
+  { title: "Event", iconName: "event", iconSource: "MaterialIcons" }, //E 5
+  { title: "Item", iconName: "box", iconSource: "Feather" }, //I 6
+  { title: "Reference", iconName: "paperclip", iconSource: "Feather" }, //R 7
+  { title: "Place", iconName: "location", iconSource: "Entypo" }, //P 8 //was Location but L was used by Log.
+  { title: "Blueprint", iconName: "flash", iconSource: "FontAwesome" }, //B 9 //was template but T conflict (with 'template' and 'rule' as sub types/classes)? - this would actually mean we had a different starting character for each type (e.g. B for Blueprints) which could help with shortcuts.
+  {
+    title: "Log",
+    iconName: "timeline-clock",
+    iconSource: "MaterialCommunityIcons",
+  }, //L 10(was conflicting with Location but ok now.)
 ];
 
 export const mapTypeMain = arrayTypeMain.map(
@@ -31,7 +34,6 @@ export const mapTypeMain = arrayTypeMain.map(
     } as any)
 );
 
-
 // TABS
 
 export const ViewTypeTabs = ({}: any) => {
@@ -44,13 +46,37 @@ export const ViewTypeTabs = ({}: any) => {
         flexDirection: "row",
       }}
     >
+      {/* 'All' button */}
+      <ViewContainerStatic style={{ flex: 1, width: 50, height: 50 }}>
+        <ViewRouterLinkthemed
+          style={{
+            padding: 5,
+            backgroundColor: 'gray'
+          }}
+          to={`entity/../../../all/display`}
+        >
+          <ViewContainerStatic style={{ alignItems: "center", flex: 1 }}>
+            <ViewIconMain
+              name={'square'}
+              source={'Feather'}
+              color={"white"}
+            />
+            <ViewTypographyText style={{ fontSize: 11 }}>
+              {`All`}
+            </ViewTypographyText>
+          </ViewContainerStatic>
+        </ViewRouterLinkthemed>
+      </ViewContainerStatic>
+      {/* The specific type buttons */}
       {arrayTypeMain?.map((x, i) => (
         <ViewContainerStatic key={i} style={{ flex: 1, width: 50, height: 50 }}>
           <ViewRouterLinkthemed
             style={{
               padding: 5,
               backgroundColor:
-                display === (x as any).title.toLowerCase() ? "gray" : "lightgray",
+                display === (x as any).title.toLowerCase()
+                  ? "gray"
+                  : "lightgray",
             }}
             to={`entity/../../../${(x as any).title.toLowerCase()}/display`}
           >
@@ -70,4 +96,3 @@ export const ViewTypeTabs = ({}: any) => {
     </ViewContainerScroll>
   );
 };
-

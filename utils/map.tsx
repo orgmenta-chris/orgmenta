777ba861dccea33b30746844c26939e1e8c5d15d
Freedom Loisa <kitakayaloisa@gmail.com>
2023-12-04 08:13:39 +0300

    created addresspicker component

--- utils/map.tsx
+++ utils/map.tsx
@@ -1,5 +1,8 @@
 import MapView from "react-native-maps";
 import { View } from "react-native";
+import { ViewContainerScroll } from "./container";
+import { useEffect, useState } from "react";
+import { ViewInputText } from "./input";
 
 export const ViewMapMobile = () => {
   const location = { latitude: 37.78825, longitude: -122.4324 };
@@ -19,6 +22,70 @@ export const ViewMapMobile = () => {
         }}
       />
     </View>
+  );
+};
+
+export interface coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+export const geoCodeAddress = async (address: any) => {
+  const res = await fetch(
+    `http://api.positionstack.com/v1/forward?access_key=1955fdf281ab2472d3c19e4c0519df53&query=${address}`
+  );
+
+  return res.json();
+};
+
+export const ViewAddressPicker = () => {
+  const [address, setAddress] = useState(
+    "1600 Pennsylvania Ave NW, Washington DC"
+  );
+
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  const location: coordinates = { latitude: 37.78825, longitude: -122.4324 };
+
+  useEffect(() => {
+    const getAddressOnMap = async () => {
+      const newAddress = await geoCodeAddress(address);
+
+      console.log(newAddress);
+    };
+
+    getAddressOnMap();
+  }, [address]);
+
+  return (
+    <ViewContainerScroll>
+      <ViewInputText
+        style={{
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+        }}
+        value={address}
+        onChangeText={setAddress}
+        placeholder="Enter address"
+      />
+
+      <MapView
+        provider="google"
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </ViewContainerScroll>
   );
 };
 

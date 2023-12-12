a28150425264bb8aabbf271333ca28f0e925a40a
Freedom Loisa <kitakayaloisa@gmail.com>
2023-12-12 05:52:57 +0300

    modified address selector component

--- utils/map.tsx
+++ utils/map.tsx
@@ -5,16 +5,21 @@ import { useEffect, useState } from "react";
 import { ViewInputText } from "./input";
 import { Map, Marker, ZoomControl } from "pigeon-maps";
 import { osm } from "pigeon-maps/providers";
+import { UtilityPlatformMain } from "./platform";
+import { ViewButtonPressable } from "./button";
+import { ViewTypographyText } from "./typography";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+// @ts-ignore
+import { STAGING_GOOGLE_PLACES_API_KEY } from "@env";
 
-export const ViewMapMobile = () => {
-  const location = { latitude: 37.78825, longitude: -122.4324 };
+export const ViewMapMobile = ({ latitude, longitude }: any) => {
   return (
     <View style={{ backgroundColor: "red", width: "100%", height: "100%" }}>
       <MapView
         provider="google"
         initialRegion={{
-          latitude: location.latitude,
-          longitude: location.longitude,
+          latitude: latitude,
+          longitude: longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
@@ -40,7 +45,7 @@ export const geoCodeAddress = async (address: any) => {
   return res.json();
 };
 
-export const WebMapView = ({ latitude, longitude }: any) => {
+export const ViewMapWeb = ({ latitude, longitude }: any) => {
   const [hue, setHue] = useState(0);
 
   const color = `hsl(${hue % 360}deg 39% 70%)`;
@@ -66,50 +71,165 @@ export const WebMapView = ({ latitude, longitude }: any) => {
 };
 
 export const ViewAddressPicker = () => {
-  const [address, setAddress] = useState(
-    "1600 Pennsylvania Ave NW, Washington DC"
-  );
+  const [address, setAddress] = useState();
 
   const [latitude, setLatitude] = useState(37.78825);
   const [longitude, setLongitude] = useState(-122.4324);
 
+  const [postcode, setPostcode] = useState("SW1A 2AB");
+  const [street, setStreet] = useState("");
+
   const location: coordinates = { latitude: 37.78825, longitude: -122.4324 };
 
-  useEffect(() => {
-    const getAddressOnMap = async () => {
-      const newAddress = await geoCodeAddress(address);
+  const getAddressOnMap = async () => {
+    const newAddress = await geoCodeAddress(address);
 
-      // console.log(newAddress);
+    // console.log(newAddress);
 
-      const newLatitude = newAddress.data[0].latitude;
-      const newLongitude = newAddress.data[0].longitude;
-      
-      setLatitude(newLatitude);
-      setLongitude(newLongitude);
+    const newLatitude = newAddress.data[0].latitude;
+    const newLongitude = newAddress.data[0].longitude;
 
-      console.log(latitude);
-      console.log(longitude);
+    setLatitude(newLatitude);
+    setLongitude(newLongitude);
+
+    console.log(latitude);
+    console.log(longitude);
+  };
+
+  const searchStreetName = async () => {
+    const url = new URL("https://uk-postcode.p.rapidapi.com/search");
+
+    const parameters = {
+      street,
     };
 
-    getAddressOnMap();
-  }, [address]);
+    url.search = new URLSearchParams(parameters).toString();
+
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "db7c4a8f07msh1f9753b83296f08p1c0a1fjsn4921f44b5d64",
+        "X-RapidAPI-Host": "uk-postcode.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+
+      const result = await response.json();
+
+      console.log(result);
+
+      setAddress(result);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const getPostCode = async () => {
+    const url = new URL("https://uk-postcode.p.rapidapi.com/getpostcode");
+
+    const parameters = {
+      postcode,
+    };
+
+    url.search = new URLSearchParams(parameters).toString();
+
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "db7c4a8f07msh1f9753b83296f08p1c0a1fjsn4921f44b5d64",
+        "X-RapidAPI-Host": "uk-postcode.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+
+      const result = await response.json();
+
+      console.log(result);
+
+      const streetName = result[0].streetName;
+
+      setStreet(streetName);
+
+      await searchStreetName();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    // getAddressFromPostCode();
+    // getAddressOnMap();
+  }, [postcode]);
 
   return (
     <ViewContainerScroll>
-      <ViewInputText
+      {/* <ViewInputText
         style={{
           height: 40,
           margin: 12,
           borderWidth: 1,
           padding: 10,
         }}
-        value={address}
-        onChangeText={setAddress}
-        placeholder="Enter address"
+        value={postcode}
+        onChangeText={setPostcode}
+        placeholder="Enter postcode"
+      /> */}
+
+      <GooglePlacesAutocomplete
+        placeholder="Type a place"
+        onPress={(data, details = null) => console.log(data, details)}
+        query={{ key: STAGING_GOOGLE_PLACES_API_KEY }}
+        fetchDetails={true}
+        onFail={(error) => console.log(error)}
+        onNotFound={() => console.log("no results")}
+        listEmptyComponent={() => (
+          <ViewContainerStatic style={{ flex: 1 }}>
+            <ViewTypographyText selectable={false}>
+              No results were found
+            </ViewTypographyText>
+          </ViewContainerStatic>
+        )}
       />
 
-      <WebMapView latitude={latitude} longitude={longitude} />
-      
+      {/* {address &&
+        // @ts-ignore
+        address.map((address: any, index: number) => {
+          return (
+            <ViewButtonPressable
+              style={{
+                flex: 1,
+                padding: 10,
+                margin: 10,
+                borderWidth: 1,
+                borderRadius: 5,
+                borderColor: "black",
+                backgroundColor: "lightblue",
+              }}
+              onPress={}
+            >
+              <ViewTypographyText
+                selectable={false}
+                style={{
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  paddingBottom: 10,
+                }}
+              >
+                {address.}
+              </ViewTypographyText>
+            </ViewButtonPressable>
+          );
+        })} */}
+
+      {UtilityPlatformMain.OS === "web" ? (
+        <ViewMapWeb latitude={latitude} longitude={longitude} />
+      ) : (
+        <ViewMapMobile latitude={latitude} longitude={longitude} />
+      )}
     </ViewContainerScroll>
   );
 };
